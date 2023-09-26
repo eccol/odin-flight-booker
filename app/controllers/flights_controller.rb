c183a217d783a1a000d73c7498d13c0cb5b95844
Eric <126840543+eccol@users.noreply.github.com>
2023-09-25 22:04:24 -0400

    Add flight search

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,2 +1,15 @@
 class FlightsController < ApplicationController
+  def index
+    @airports = Airport.pluck(:code)
+    @dates = Flight.pluck(:date)
+
+    @results = Flight.where(search_params) if params.has_key?(:departure_airport)
+  end
+
+  private
+  def search_params
+    {departure_airport: Airport.find_by(code: params[:departure_airport]),
+    arrival_airport: Airport.find_by(code: params[:arrival_airport]),
+    date: params[:date]}
+  end
 end
