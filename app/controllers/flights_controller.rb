7d2e5845e4a33674fdbf8f60881107512bf3d9a7
Eric <126840543+eccol@users.noreply.github.com>
2023-09-26 15:07:57 -0400

    Simplify airport selects

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,6 +1,6 @@
 class FlightsController < ApplicationController
   def index
-    @airports = Airport.pluck(:code)
+    @airports = Airport.all
     @dates = Flight.pluck(:date)
 
     @results = Flight.where(search_params) if params.has_key?(:departure_airport)
@@ -8,8 +8,8 @@ class FlightsController < ApplicationController
 
   private
   def search_params
-    {departure_airport: Airport.find_by(code: params[:departure_airport]),
-    arrival_airport: Airport.find_by(code: params[:arrival_airport]),
+    {departure_airport: Airport.find(params[:departure_airport]),
+    arrival_airport: Airport.find(params[:arrival_airport]),
     date: params[:date]}
   end
 end
