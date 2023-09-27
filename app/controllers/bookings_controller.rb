4e04fbb86c0cb48a0876481d9c7d896c616ff143
Eric <126840543+eccol@users.noreply.github.com>
2023-09-26 20:05:00 -0400

    Create booking form

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,4 +2,8 @@ class BookingsController < ApplicationController
   def new
     @booking = Flight.find(params[:result_id]).bookings.new(num_passengers: params[:passenger_count])
   end
+
+  def create
+    
+  end
 end
