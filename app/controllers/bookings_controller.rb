2251e43c2b3fafa2071a7119010b4372a1a1632a
Eric <126840543+eccol@users.noreply.github.com>
2023-09-26 21:28:40 -0400

    Implement creating booking with passenger names

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,9 +1,24 @@
 class BookingsController < ApplicationController
   def new
     @booking = Flight.find(params[:result_id]).bookings.new(num_passengers: params[:passenger_count])
+    @booking.num_passengers.times do |i|
+      @booking.passengers.build
+    end
   end
 
   def create
-    
+    @booking = Booking.new(booking_params)
+
+    if @booking.save
+      redirect_to @booking
+    else
+      flash.now[:error] = "Could not create booking."
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def booking_params
+    params.require(:booking).permit(:flight_id, passengers_attributes: [:id, :name, :email])
   end
 end
